1f5c594b03e1b21754a91f2fc68af48d82e5041d
GeraldAlexi <137075335+GeraldAlexi@users.noreply.github.com>
2023-11-17 14:05:33 -0300

    Update guardar_2.php

--- taller/agregar/guardar_2.php
+++ taller/agregar/guardar_2.php
@@ -1,38 +1,62 @@
 <?php 
-include ('../loginfl/database.php');
+require ('../loginfl/database.php');
 
 $nombre=$_POST['nombre'];
 $stock=$_POST['stock'];
+$descrip=$_POST['descrip'];
 
 $nom= strtolower($nombre);
+$descrip= strtolower($descrip);
 
-$validar=mysqli_query($conn,"SELECT nombre from productos WHERE nombre='$nom'");
-$r=mysqli_num_rows($validar);
-if($r>0){
 
-    $modificar=mysqli_query($conexion,"UPDATE productos set stock=stock+'$stock'
-            WHERE nombre='$nom'");
-
-    echo
-        '<script> 
-        location.href="guardar.php";
-        </script>;
-    ';
+if (!empty($nombre) && !empty($stock) && !empty($descrip)){
+$validar=$conn->prepare("SELECT nombre from productos WHERE nombre=:nom and descripcion=:descrip");
+$validar->bindParam(':nom', $nom);
+$validar->bindParam(':descrip', $descrip);
+$validar->execute();
+$r = $validar->fetch(PDO::FETCH_ASSOC);
+if($r > 0){
+    modi($nom,$descrip,$stock,$conn);
 }
-elseif($r=0){
-    $insertar= mysqli_query($conexion,"INSERT INTO productos (nombre,stock,estado) 
-    VALUES ('$nom','$stock','no prestado')");
+else {
+    inser($nom,$descrip,$stock,$conn);
+
+}
+
+
+function modi($n,$des,$sto,$con){
+
+    $modificar=$con->prepare("UPDATE productos set stock=stock + :stock WHERE nombre=:nom and descripcion=:descrip");
+    $modificar->bindParam(':nom', $n);
+    $modificar->bindParam(':stock', $sto);
+    $modificar->bindParam(':descrip', $des);
+    $modificar->execute();
+    header("Location:../formulario.php");
+    exit();
+
+}
+function inser($n,$des,$sto,$con){
+
+    $insertar=$con->prepare("INSERT INTO productos (nombre,stock,descripcion) VALUES (:nombre, :stock, :descrip)");
+    $insertar->bindParam(':nombre', $n);
+    $insertar->bindParam(':stock', $sto);
+    $insertar->bindParam(':descrip', $des);
+    $insertar->execute();
 
     if($insertar){
-        echo
-            '<script> 
-            location.href="guardar.php";
-            </script>;
-        ';
 
+        header("Location:../formulario.php");
+        exit();
     }
 }
+}
+else{
+    header("Location: ../formulario.php?error=Faltan datos por agregar");
+    exit();
 
-mysqli_close($conexion);
-// mysqli_close($conexion);
+}
+
+
+
+mysqli_close($conn);
 ?>
